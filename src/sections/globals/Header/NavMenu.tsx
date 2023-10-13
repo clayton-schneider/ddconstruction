957608f9468cc02eb2b66c08977e408c4e79b116
clayton-schneider <schneiderc2016@gmail.com>
2023-10-13 16:46:48 -0400

    header fixes for styling

--- src/sections/globals/Header/NavMenu.tsx
+++ src/sections/globals/Header/NavMenu.tsx
@@ -22,7 +22,7 @@ const NavMenu = ({ links }: Props) => {
     <div>
       <ul
         className={
-          "font-bebas absolute top-[100px] z-50 flex w-full flex-col items-start gap-6 bg-white px-4 py-5 text-xl text-white transition-all duration-300 xl:static xl:w-auto xl:flex-row xl:items-center xl:bg-transparent xl:px-0 xl:py-0 " +
+          "font-bebas lg:text-text-darktransition-all absolute top-[83px] z-50 flex w-full flex-col items-start gap-6 bg-white px-4 py-5 text-xl text-text-dark duration-300 lg:text-white xl:static xl:w-auto xl:flex-row xl:items-center xl:bg-transparent xl:px-0 xl:py-0 " +
           (isOpen ? "left-0 z-40 shadow shadow-neutral-200" : "-left-full")
         }
       >
@@ -31,11 +31,11 @@ const NavMenu = ({ links }: Props) => {
             return (
               <li key={idx} className="group relative">
                 <a href={link.link}>{link.linkText}</a>
-                <ul className="z-50 w-full pl-5 pt-1 text-center text-black lg:absolute lg:left-1/2 lg:hidden lg:w-[200px] lg:-translate-x-1/2 lg:pl-0  lg:shadow lg:group-hover:block">
+                <ul className="z-50 w-full pl-5 pt-1 text-center lg:absolute lg:left-1/2 lg:hidden lg:w-[200px] lg:-translate-x-1/2 lg:pl-0  lg:shadow lg:group-hover:block">
                   {link.sublinks.map((sublink, idx) => (
                     <li
                       key={idx}
-                      className="text-left first:rounded-t last:rounded-b lg:bg-white lg:text-center lg:hover:bg-primary lg:hover:text-white"
+                      className="text-left text-text-dark transition-all duration-200 first:rounded-t last:rounded-b lg:bg-white lg:text-center lg:hover:bg-dark lg:hover:text-white"
                     >
                       <a className="block px-4 py-2" href={sublink.link}>
                         {sublink.linkText}
