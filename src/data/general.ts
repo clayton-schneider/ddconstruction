d6cdf9f2ce3ad760744ec754871758acb5d7c623
clayton-schneider <schneiderc2016@gmail.com>
2023-10-13 10:49:31 -0400

    initial setup commit

--- src/data/general.ts
+++ src/data/general.ts
@@ -1,12 +1,9 @@
 export const socials = [
-  { name: "facebook", icon: "mdi:facebook", link: "https://www.facebook.com/" },
-
   {
     name: "instagram",
     icon: "mdi:instagram",
-    link: "https://www.instagram.com/",
+    link: "https://www.instagram.com/d_dfamily_construction/",
   },
-  { name: "youtube", icon: "mdi:youtube", link: "https://www.youtube.com/" },
 ];
 
 export const hours = [
@@ -15,16 +12,14 @@ export const hours = [
   { day: "Wednesday", open: "9:00", close: "5:00" },
   { day: "Thursday", open: "9:00", close: "5:00" },
   { day: "Friday", open: "9:00", close: "5:00" },
-  { day: "Saturday", open: "9:00", close: "5:00" },
-  { day: "Sunday", open: "9:00", close: "5:00" },
 ];
 
 export const contacts = [
   {
     name: "Address",
-    text: "1 Main Street, Boston, MA 02134",
+    text: "935 Kenmore Rd, Philadelphia, PA 19151",
     icon: "mdi:pin",
   },
-  { name: "Email", text: "contact@website.com", icon: "mdi:email" },
-  { name: "Phone", text: "800-000-000", icon: "mdi:phone" },
+  { name: "Email", text: "dw@d-dfamilyconstructionllc.com", icon: "mdi:email" },
+  { name: "Phone", text: "215-971-0337", icon: "mdi:phone" },
 ];
